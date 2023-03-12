e36e8b40cd75c25443ac21b2b2d015615ebee6d7
Bogdan Rubakov ITI-22 <96008916+bende-r@users.noreply.github.com>
2023-03-12 17:02:12 +0300

    Delete Lab_1/Lab_1 directory

--- Lab_1/Lab_1/src/MenuLib/Main.java
+++
@@ -1,2 +0,0 @@
-package MenuLib;public class Main {
-}
--- Lab_1/Lab_1/src/MenuLib/Toasts.java
+++
@@ -1,7 +0,0 @@
-package MenuLib;
-
-public  class Toasts extends Dish {
-    public Toasts(String name, float coast, int calories, boolean veganFriendly) {
-        super(name, coast, calories, veganFriendly);
-    }
-}
