ba7468ba198d684d987194cd3a5c41b1d76c3bda
Bogdan Rubakov ITI-22 <96008916+bende-r@users.noreply.github.com>
2023-03-12 17:02:41 +0300

    Delete Lab_1/Lab_1 directory

--- Lab_1/Lab_1/src/MenuLib/Drinks.java
+++
@@ -1,7 +0,0 @@
-package MenuLib;
-
-public class Drinks extends Dish {
-    public Drinks(String name, float coast, int calories, boolean veganFriendly) {
-        super(name, coast, calories, veganFriendly);
-    }
-}
